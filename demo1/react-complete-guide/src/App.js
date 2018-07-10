daa3b6680187d2da0eb6824031e9ac71aa3cd2e9
Do Ngoc Nam <noface151151@gmail.com>
2018-07-10 15:00:06 +0700

    commit basic

--- demo1/react-complete-guide/src/App.js
+++ demo1/react-complete-guide/src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons:[
+      {name: "Max",age:28},
+      {name: "Manu",age:29},
+      {name: "Stephanie",age:26}
+    ]
+  }
+
+  switchNameHandler=(newName)=>{
+    //console.log('Clicked');
+      this.setState({ persons:[
+        {name: newName,age:28},
+        {name: "Manu",age:29},
+        {name: "Stephanie",age:26}
+      ]
+    });
+  }
+
+  nameChangeHandler=(event)=>{
+      this.setState({ persons:[
+        {name: "Max",age:28},
+        {name: event.target.value,age:29},
+        {name: "Stephanie",age:26}
+      ]
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={()=>this.switchNameHandler('Nam')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,'Toan')}
+          changed={this.nameChangeHandler}>My hobbies: Racing</Person>
+          
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+         // click={this.switchNameHandler.bind(this,'Cuong')}
+          />
       </div>
     );
+  //  return React.createElement('div',{className:'App'},React.createElement('h1',null,'I\'m a React app'));
   }
 }
 
