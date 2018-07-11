bfc75f907415160e27dbf923366ee32a6729bc88
Do Ngoc Nam <noface151151@gmail.com>
2018-07-11 13:35:51 +0700

    commit 3

--- demo1/react-complete-guide/src/components/Persons/Person/Person.js
+++ demo1/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import PropTypes from 'prop-types';
 
 
 class Person extends Component{
@@ -11,7 +12,8 @@ class Person extends Component{
         console.log('[Person.js] componentWillMount()')
       }
       componentDidMount(){
-        console.log('[Person.js] componentDidMount()')
+        console.log('[Person.js] componentDidMount()');
+        this.inputElement.focus();
       }
     render(){
         console.log('[Person.js]  render()')
@@ -19,9 +21,16 @@ class Person extends Component{
             <div>
                 <p onClick={this.props.click}> I 'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p> {this.props.children} </p>
-                <input type="text" onChange={this.props.changed} />
+                <input type="text" onChange={this.props.changed} ref={(inp)=>{this.inputElement=inp}}/>
             </div>
         ); 
     }
 }
+
+Person.propTypes={
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 export default Person;
